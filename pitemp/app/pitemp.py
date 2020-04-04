3dd1c2f3488d5715ac110f69678d16f2c3b981ed
Jonathan Arrance <jonathan.arrance@gmail.com>
2020-04-04 16:20:34 -0400

    checkpoint

--- pitemp/app/pitemp.py
+++ pitemp/app/pitemp.py
@@ -2,3 +2,12 @@
 #rpi based temp monitoring and alerting
 import pitemp_lib
 import settings
+
+#lets get the temps off of the DHT11 sensors
+
+#lets print the output of the sensors
+
+#if there is an overheat situation blink the led
+
+#lets keep an eye on the button and change output from stats to temps
+
