02c827778f231453262e86b45eeae1faa2c675bf
Jonathan Arrance <jonathan.arrance@gmail.com>
2020-04-04 22:25:27 -0400

    first commit of DHT11,

--- pitemp/app/pitemp.py
+++ pitemp/app/pitemp.py
@@ -1,11 +1,29 @@
 #!/bin/python
 #rpi based temp monitoring and alerting
-import pitemp_lib
-import settings
+import time
+import board
+import adafruit_dht
 
-#lets get the temps off of the DHT11 sensors
+#Pins where DHT11 sensors connected
+pins = ['22','23','24']
 
 #lets print the output of the sensors
+while True:
+    for pin in pins:
+        dhtDevice = adafruit_dht.DHT11(board.D%s)%(pin)
+        try:
+            temperature_c = dhtDevice.emperature
+            temperature_f = temperature_c * (9 / 5) + 32
+            humidity = dhtDevice.humidity
+            print(
+                "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                    temperature_f, temperature_c, humidity
+                )
+            )
+        except RuntimeError as error:
+            print(error.args[0])
+    time.sleep(2)
+
 
 #if there is an overheat situation blink the led
 
