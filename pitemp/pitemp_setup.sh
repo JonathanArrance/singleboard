c8972c31367d35f1f368288944f8bb58f5573ed8
Jonathan Arrance <jonathan.arrance@gmail.com>
2020-04-04 20:54:29 -0400

    Added in some build information

--- pitemp/pitemp_setup.sh
+++ pitemp/pitemp_setup.sh
@@ -1,1 +1,22 @@
-apt-get -y update
+#!/bin/bash
+sudo apt-get -y update
+sudo apt-get upgrade
+
+sudo apt-get install python3-pip
+sudo apt install -y python3-dev
+sudo apt install -y python-imaging python-smbus i2c-tools
+sudo apt install -y python3-pil
+sudo apt install -y python3-pip
+sudo apt install -y python3-setuptools
+#sudo apt install -y python3-rpi.gpio
+
+sudo pip3 install --upgrade setuptools
+
+sudo `echo 'dtoverlay=spi1-3cs' `>> /boot/config.txt
+
+sudo pip3 install RPI.GPIO
+sudo pip3 install adafruit-blinka
+sudo pip3 install adafruit-circuitpython-dht
+sudo pip3 install schedule
+
+sudo apt-get install libgpiod2
