b726e968fd433dcdae30d7233fad62798df95096
Jonathan Arrance <jonathan.arrance@gmail.com>
2020-04-10 19:17:36 -0400

    fixed the loop

--- pitemp/app/pitemp.py
+++ pitemp/app/pitemp.py
@@ -6,13 +6,14 @@ import Adafruit_DHT
 import settings
 
 #get the sensor
-sensor = 'Adafruit_DHT.'+settings.SENSORTYPE
-
+#sensor = 'Adafruit_DHT.'+settings.SENSORTYPE
+sensor = Adafruit_DHT.DHT11
 #Pins where DHT11 sensors connected
-pins = ['22','23','4']
+pins = ['24','23','4']
 
 #lets print the output of the sensors
 while True:
+    print("------")
     for pin in pins:
         try:
             humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
@@ -20,12 +21,11 @@ while True:
             if(settings.SCALE == 'Fahrenheit'):
                 temperature = temperature * 9/5.0 + 32
                 temp_scale = 'F'
-            humidity = dhtDevice.humidity
             print( "Temp: {:.1f} {}   Humidity: {}% ".format(temperature, temp_scale, humidity))
         except RuntimeError as error:
             print(error.args[0])
-    time.sleep(2)
-
+    print("---------")
+    time.sleep(1)
 
 #if there is an overheat situation blink the led
 
