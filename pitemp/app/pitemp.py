4891c8c90ca9ded16ae3660a79148cbad3031bb1
Jonathan Arrance <jonathan.arrance@gmail.com>
2020-04-18 16:56:58 -0400

    Changed to class implement schedule

--- pitemp/app/pitemp.py
+++ pitemp/app/pitemp.py
@@ -14,21 +14,80 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-class PiTemp():
 
-    def __init__(self):
-        pass
+'''
+def temp_sensor():
+    #get the sensor
+    #sensor = 'Adafruit_DHT.'+settings.SENSORTYPE
+    sensor = Adafruit_DHT.DHT11
+    #Pins where DHT11 sensors connected
+    pins = settings.PINS
 
-    def temp_sensor(self):
-        #get the sensor
-        #sensor = 'Adafruit_DHT.'+settings.SENSORTYPE
-        sensor = Adafruit_DHT.DHT11
-        #Pins where DHT11 sensors connected
-        pins = settings.PINS
+    #lets print the output of the sensors
+    self.output = {}
+    for pin in pins:
+        try:
+            humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+            temp_scale = 'C'
+            if(settings.SCALE == 'Fahrenheit'):
+                temperature = temperature * 9/5.0 + 32
+                temp_scale = 'F'
+            self.output['temp_pin%s'%(pin)] = temperature
+            self.output['temp_scale'] = temp_scale
+            self.output['humidity_pin%s'%(pin)] = humidity
+            #print( "Temp: {:.1f} {} Humidity: {}% ".format(temperature, temp_scale, humidity))
+        except RuntimeError as error:
+            print(error.args[0])
+    print(self.output)
+    return self.output
+'''
 
+
+def screen_output():
+    #get the pins
+    PIN = settings.PINS
+    sensor = Adafruit_DHT.DHT11
+    
+    if(settings.SCALE=='Fahrenheit'):
+        temp_scale = 'F'
+    else:
+        temp_scale = 'C'
+    
+    # 128x32 display with hardware I2C:
+    disp = Adafruit_SSD1306.SSD1306_128_64(rst=settings.RST)
+
+    # Initialize library.
+    disp.begin()
+
+    # Clear display.
+    disp.clear()
+    disp.display()
+
+    width = disp.width
+    height = disp.height
+    image = Image.new('1', (width, height))
+
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+    # Draw some shapes.
+    # First define some constants to allow easy resizing of shapes.
+    padding = -2
+    top = padding
+    bottom = height-padding
+    # Move left to right keeping track of the current x position for drawing shapes.
+    x = 0
+
+    # Load default font.
+    font = ImageFont.load_default()
+
+    while True:
         #lets print the output of the sensors
         self.output = {}
-        for pin in pins:
+        for pin in PIN:
             try:
                 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
                 temp_scale = 'C'
@@ -41,69 +100,22 @@ class PiTemp():
                 #print( "Temp: {:.1f} {} Humidity: {}% ".format(temperature, temp_scale, humidity))
             except RuntimeError as error:
                 print(error.args[0])
-        print(self.output)
-        return self.output
-
-
-    
-    def screen(self):
-        #get the pins
-        PIN = settings.PINS
         
-        if(settings.SCALE=='Fahrenheit'):
-            temp_scale = 'F'
-        else:
-            temp_scale = 'C'
-        
-        # 128x32 display with hardware I2C:
-        disp = Adafruit_SSD1306.SSD1306_128_64(rst=settings.RST)
-
-        # Initialize library.
-        disp.begin()
-
-        # Clear display.
-        disp.clear()
-        disp.display()
-
-        width = disp.width
-        height = disp.height
-        image = Image.new('1', (width, height))
-
-        # Get drawing object to draw on image.
-        draw = ImageDraw.Draw(image)
-
         # Draw a black filled box to clear the image.
         draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-        # Draw some shapes.
-        # First define some constants to allow easy resizing of shapes.
-        padding = -2
-        top = padding
-        bottom = height-padding
-        # Move left to right keeping track of the current x position for drawing shapes.
-        x = 0
+        draw.text((x, top),       "PiTemp.", font=font, fill=255)
+        draw.text((x, top+16),    "S1: "+str(sensor_input['temp_pin%s'%(str(PIN[0]))])+" "+sensor_input['temp_scale']+"Humidity "+str(sensor_input['humidity_pin%s'%(str(PIN[0]))]), font=font, fill=255)
+        #draw.text((x, top+16),    "Sensor1: Temp %s %s Humidity %s"%(sensor_input['temp_pin%s']%(str(PIN[0])),sensor_input['temp_scale'],sensor_input['humidity_pin%s']%(str(PIN[0]))), font=font, fill=255)
+        #draw.text((x, top+24),    "Sensor2: Temp %s %s Humidity %s"%(sensor_input['temp_pin%s']%(str(PIN[1])),sensor_input['temp_scale'],sensor_input['humidity_pin%s']%(str(PIN[1]))),  font=font, fill=255)
+        #draw.text((x, top+32),    "Sensor3: Temp %s %s Humidity %s"%(sensor_input['temp_pin%s']%(str(PIN[2])),sensor_input['temp_scale'],sensor_input['humidity_pin%s']%(str(PIN[2]))),  font=font, fill=255)
 
-        # Load default font.
-        font = ImageFont.load_default()
-
-        while True:
-            # Draw a black filled box to clear the image.
-            draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-            draw.text((x, top),       "PiTemp.", font=font, fill=255)
-            draw.text((x, top+16),    "S1: "+str(sensor_input['temp_pin%s'%(str(PIN[0]))])+" "+sensor_input['temp_scale']+"Humidity "+str(sensor_input['humidity_pin%s'%(str(PIN[0]))]), font=font, fill=255)
-            #draw.text((x, top+16),    "Sensor1: Temp %s %s Humidity %s"%(sensor_input['temp_pin%s']%(str(PIN[0])),sensor_input['temp_scale'],sensor_input['humidity_pin%s']%(str(PIN[0]))), font=font, fill=255)
-            #draw.text((x, top+24),    "Sensor2: Temp %s %s Humidity %s"%(sensor_input['temp_pin%s']%(str(PIN[1])),sensor_input['temp_scale'],sensor_input['humidity_pin%s']%(str(PIN[1]))),  font=font, fill=255)
-            #draw.text((x, top+32),    "Sensor3: Temp %s %s Humidity %s"%(sensor_input['temp_pin%s']%(str(PIN[2])),sensor_input['temp_scale'],sensor_input['humidity_pin%s']%(str(PIN[2]))),  font=font, fill=255)
-
-            # Display image.
-            disp.image(image)
-            disp.display()
+        # Display image.
+        disp.image(image)
+        disp.display()
 
 if __name__=='__main__':
-
-    pt = PiTemp()
-    schedule.every(settings.INTERVAL).seconds.do(pt.temp_sensor())
+    schedule.every(settings.INTERVAL).seconds.do(screen_output())
 
     #while True:
         #get the temp from the sensors
